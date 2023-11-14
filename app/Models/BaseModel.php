963a07fe5c7959abccaeac4f31c1952742b876a9
Agung Kurniawan <agungkes95@gmail.com>
2023-11-14 21:17:24 +0700

    feat: add query cache

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -8,34 +8,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Scout\Searchable;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 use RichanFongdasen\EloquentBlameable\BlameableTrait;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
-/**
- * App\Models\BaseModel
- *
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel createdBy($userId)
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel query()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel updatedBy($userId)
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel withTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|BaseModel withoutTrashed()
- *
- * @property-read \Modules\User\Entities\User|null $creator
- * @property-read \Modules\User\Entities\User|null $updater
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $activities
- * @property-read int|null $activities_count
- * @property-read string|null $hash_id
- * @property-read string $hash_id_raw
- *
- * @mixin \Eloquent
- */
 class BaseModel extends Model
 {
-    use BlameableTrait, HasFactory, HasHashId, HasHashIdRouting, LogsActivity, Searchable, SoftDeletes;
+    use BlameableTrait, HasFactory, HasHashId, HasHashIdRouting, LogsActivity, QueryCacheable, Searchable, SoftDeletes;
+
+    public $cacheFor = 3600;
+
+    protected static $flushCacheOnUpdate = true;
 
     public function getActivitylogOptions(): LogOptions
     {
