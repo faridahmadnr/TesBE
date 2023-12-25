3913918b624afb700be4749635af834c37650166
Agung Kurniawan <agungkes95@gmail.com>
2023-12-25 15:32:59 +0700

    fix: custom error page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>403 Error</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="container">
+        <h2>403, forbidden.</h2>
+    </div>
+</body>
+</html>
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 Error</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="container">
+        <h2>404, page not found.</h2>
+    </div>
+</body>
+</html>
