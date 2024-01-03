a058acfd3f72eeb6cd3a4481b068c254071dfc7b
Agung Kurniawan <agungkes95@gmail.com>
2024-01-03 21:19:58 +0700

    fix: add signature request

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\LogRoute;
+use App\Http\Middleware\VerifySignature;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -42,6 +43,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             LogRoute::class,
+            VerifySignature::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
---
+++ app/Http/Middleware/VerifySignature.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifySignature
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $sign = $request->header('X-Sign');
+        $signTimestamp = $request->header('X-Sign-Timestamp');
+
+        if (! $this->isValidTimestamp($signTimestamp)) {
+            return response()->json(['error' => 'Invalid timestamp'], 401);
+        }
+
+        $backendSign = $this->generateBackendSignature(
+            $request,
+            $signTimestamp
+        );
+        // Log::info("Frontend: $sign From backend: $backendSign");
+        if ($sign !== $backendSign) {
+            return response()->json(['error' => 'Invalid signature'], 401);
+        }
+
+        return $next($request);
+    }
+
+    private function isValidTimestamp($timestamp)
+    {
+        if ($timestamp && (time() - $timestamp <= 5)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function generateBackendSignature(Request $request, $timestamp)
+    {
+        $md5 = md5(json_encode([
+            'url' => rawurlencode($request->url()),
+            'method' => $request->method(),
+            'requestId' => $request->header('X-Request-Id', ''),
+            'xsrfToken' => $request->header('X-Xsrf-Token', ''),
+            'timestamp' => $timestamp,
+        ]));
+        $string = $request->method().$request->url().$timestamp.$md5;
+
+        Log::info($request->url());
+        $hash = hash_hmac('SHA256', $string, config('app.key'));
+
+        return base64_encode($hash);
+    }
+}
