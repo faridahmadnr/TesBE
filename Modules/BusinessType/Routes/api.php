beb9cb93dee05572a177c15fa0b684e884645cf9
Agung Kurniawan <agungkes95@gmail.com>
2024-01-06 10:46:40 +0700

    fix: using new macro

--- Modules/BusinessType/Routes/api.php
+++ Modules/BusinessType/Routes/api.php
@@ -13,25 +13,4 @@ use Modules\BusinessType\Http\Controllers\API\V1;
 |
 */
 
-Route::group(['prefix' => 'v1', 'as' => 'api.v1.'], function () {
-    Route::apiResource('business-types', V1\BusinessTypeController::class)
-        ->parameters([
-            'business-types' => 'businessType',
-        ])
-        ->only(['index']);
-    Route::group(['middleware' => 'auth:sanctum'], function () {
-        Route::controller(V1\BusinessTypeController::class)->group(function () {
-            Route::post('business-types/{businessType}/restore', 'restore')
-                ->name('business-types.restore')
-                ->withTrashed();
-            Route::delete('business-types/{businessType}/delete', 'forceDelete')
-                ->name('business-types.delete')
-                ->withTrashed();
-        });
-        Route::apiResource('business-types', V1\BusinessTypeController::class)
-            ->parameters([
-                'business-types' => 'businessType',
-            ])
-            ->except(['index']);
-    });
-});
+Route::apiRoutes('business-types', V1\BusinessTypeController::class);
