727a9b2857dcf5c91e1fc151ba3be37bc2852743
Agung Kurniawan <agungkes95@gmail.com>
2024-04-21 21:57:18 +0700

    fix: add docblock to sector report

--- Modules/Report/Entities/SectorReport.php
+++ Modules/Report/Entities/SectorReport.php
@@ -9,6 +9,20 @@ use Modules\BusinessType\Entities\BusinessType;
 /**
  * Modules\Report\Entities\SectorReport
  *
+ * @property int $id
+ * @property int|null $business_type_id
+ * @property string $date
+ * @property int $debtor_value
+ * @property int $contract_value
+ * @property int $outstanding_value
+ * @property int $target
+ * @property int $realization
+ * @property int|null $created_by
+ * @property int|null $updated_by
+ * @property int|null $deleted_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Spatie\Activitylog\Models\Activity> $actions
  * @property-read int|null $actions_count
  * @property-read BusinessType|null $businessType
@@ -18,14 +32,30 @@ use Modules\BusinessType\Entities\BusinessType;
  * @property-read mixed $month
  * @property-read mixed $year
  * @property-read \Modules\User\Entities\User|null $updater
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel createdBy($userId)
  * @method static \Illuminate\Database\Eloquent\Builder|SectorReport newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|SectorReport newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|SectorReport onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|SectorReport query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel updatedBy($userId)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereBusinessTypeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereContractValue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereDebtorValue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereDeletedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereOutstandingValue($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereRealization($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereTarget($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|SectorReport whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|SectorReport withTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|SectorReport withoutTrashed()
+ *
  * @mixin \Eloquent
  */
 class SectorReport extends BaseModel
