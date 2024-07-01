821bdc9827e6bcc8dfa65f76992b91f4d64de017
Agung Kurniawan <agungkes95@gmail.com>
2024-07-01 12:49:32 +0700

    fix: remove unused method

--- app/Console/Commands/MigrateData.php
+++ app/Console/Commands/MigrateData.php
@@ -33,7 +33,6 @@ class MigrateData extends Command
         $this->info('Start Migrate Data');
 
         $this->migrateUser();
-        $this->migrateCreditRequest();
 
         $this->info('Finish Migrate Data');
     }
