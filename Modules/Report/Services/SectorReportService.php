ac298bebe28075b84beae226df0931e8ad40ff9b
Agung Kurniawan <agungkes95@gmail.com>
2024-11-25 21:13:43 +0700

    fix: change group by name

--- Modules/Report/Services/SectorReportService.php
+++ Modules/Report/Services/SectorReportService.php
@@ -163,7 +163,7 @@ final class SectorReportService extends BaseService
             ->selectRaw('SUM(COALESCE(CASE WHEN credit_requests.status != '.CreditRequestStatusEnum::APPROVED->value.' THEN credit_requests.amount ELSE 0 END, 0)) AS submission')
             ->selectRaw('SUM(COALESCE(CASE WHEN credit_requests.status = '.CreditRequestStatusEnum::APPROVED->value.' AND '.DB::regexp('credit_requests.remark', '^[0-9]+$').' THEN CAST(credit_requests.remark AS decimal) ELSE 0 END, 0)) AS realization')
             ->orderBy('business_types.id')
-            ->groupBy('business_types.id');
+            ->groupBy('business_types.name');
 
         return $query->get()->map(function ($item) {
             return [
