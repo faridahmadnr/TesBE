512332a023f8656608266feae9b1ed830a09e4ce
Agung Kurniawan <agungkes95@gmail.com>
2025-03-12 15:38:01 +0700

    fix: temporary disabled signature request

--- app/Http/Middleware/VerifySignature.php
+++ app/Http/Middleware/VerifySignature.php
@@ -24,7 +24,7 @@ class VerifySignature
             return $next($request);
         }
 
-        // return $next($request);
+        return $next($request);
         if (! $this->isValidTimestamp($signTimestamp)) {
             if ($isDebug) {
                 return response()->json([
